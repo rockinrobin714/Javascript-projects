774d203d01b473643cdf2da10f55e47f55c42407
rockinrobin714 <robindykema@gmail.com>
2017-01-15 20:37:44 -0600

    Erase all now works

--- calculator/js/javascript.js
+++ calculator/js/javascript.js
@@ -9,7 +9,8 @@ $('.num').click(function(){
 	while (currentString[0]==='0' && currentString.length>1){
 		currentString=currentString.substr(1);
 	}
-  $('#current-equation').html(currentString);
+	//only show the last 11 numbers on the screen
+  $('#current-equation').html(currentString.slice(-11));
 })
 
 $('#erase-one').click(function(){
@@ -24,14 +25,22 @@ $('#erase-one').click(function(){
 	if (currentString===''){
 		currentString = '0';
 	}
-	$('#current-equation').html(currentString);
+	//only show the last 10 numbers on the screen
+  $('#current-equation').html(currentString.slice(-11));
 })
 
 $('.operator').click(function(){
-	currentString += $(this).html().replace(/^\s+|\s+$|\s+(?=\s)/g, "");
+	currentString += ' ' + $(this).html().replace(/^\s+|\s+$|\s+(?=\s)/g, "")+ ' ';
+	//only show the last 10 numbers on the screen
+  $('#current-equation').html(currentString.slice(-11));
 })
 
 //check phone to see how this works
+$('#erase-all').click(function(){
+	currentString = '';
+	$('#current-equation').html('0');
+})
+
 $('#erase-all').click(function(){
 	
 })
