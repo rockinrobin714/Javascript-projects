3476e57035ee28640acbab42bea86b8f662dd618
rockinrobin714 <robindykema@gmail.com>
2017-01-16 14:49:54 -0600

    set up players and turns

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,5 +1,17 @@
+var rl = require('readline');
+
+function ask(question, callback) {
+  var r = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout});
+  r.question(question + '\n', function(answer) {
+    r.close();
+    callback(answer);
+  });
+}
+
 var board = 
-	'? ? ? \n? ? ?\n? ? ?'
+	'1? 2? 3? \n4? 5? 6?\n7? 8? 9?'
 
 var turn = 'player1';
 
@@ -12,3 +24,15 @@ var newTurn = function(){
 }	
 
 console.log("Let's play!");
+ask('Who will go first? Player 1 or player 2?', function(answer){
+	if (answer.includes('1')){
+		turn = 'player1';
+		console.log('Player one is going first!')
+	} else if (answer.includes('2')){
+		turn = 'player2';
+		console.log('Player two is going first!')
+	} else {
+		turn = 'player1';
+		console.log('Sorry, did not catch that. Player 1 can go first')
+	}
+});
