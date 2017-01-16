6698578afa269f675643497639e994323eaf74f9
rockinrobin714 <robindykema@gmail.com>
2017-01-16 14:42:46 -0600

    set up board

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+var board = 
+	'? ? ? \n? ? ?\n? ? ?'
+
+var turn = 'player1';
+
+var newTurn = function(){
+	if (turn === 'player1'){
+		turn = 'player2'
+	} else {
+		turn = 'player1'
+	}
+}	
+
+console.log("Let's play!");
