f9b7328174c0fc0da57865382fa4f31cbf2aa698
rockinrobin714 <robindykema@gmail.com>
2017-01-16 15:10:53 -0600

    Swapping turns

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -14,27 +14,40 @@ var board =
 	' |1|2|3|\n_________\n |4|5|6|\n_________\n |7|8|9|'
 
 var turn = {current: null, letter: null}
-var player1;
-var player2;
-
+var winner = null;
 var newTurn = function(){
-	if (turn === 'Player 1'){
-		turn = 'Player 2'
+	if (turn.current === 'Player 1'){
+		turn.current = 'Player 2'
 	} else {
-		turn = 'Player 1'
+		turn.current = 'Player 1'
+	}
+	if (turn.letter === "X"){
+		turn.letter = "O";
+	} else{
+		turn.letter = "X";
 	}
 }	
 var choose = function(){
 	ask(`${turn.current}, do you want 'X' or 'O'?`,function(answer){
-		if (answer === 'X' || answer === 'x')
+		if (answer === 'X' || answer === 'x'){
+			console.log("You are X!")
+			turn.letter = 'X';
+		} else if (answer ==='O' || answer === 'o'){
+			console.log("You are O!")
+			turn.letter = 'O';
+
+		} else {
+			console.log("Not sure what you were typing there. I will just make you X.")
+			turn.letter = 'X';
 		}
+		playGame();
 	})
 }
 var play = function(){
 	
 }
 var playGame = function(){
-	ask(`${board}\n ${turn.current}, make your move. Type a number 1 - 9 `, function(answer){
+	ask(`${board}\n ${turn.current}, make your move. Type a number 1 - 9.`, function(answer){
 		if (parseInt(answer)!=answer)	{
 			console.log("That was not a number. Try again.")
 			playGame();
@@ -46,14 +59,12 @@ ask('Who will go first? Player 1 or player 2?', function(answer){
 	if (answer.includes('1')){
 		turn.current = 'Player 1';
 		console.log('Player one is going first!')
-		playGame();
 	} else if (answer.includes('2')){
 		turn.current = 'Player 2';
 		console.log('Player two is going first!')
-		playGame();
 	} else {
-		turn = 'Player 1';
+		turn.current = 'Player 1';
 		console.log('Sorry, did not catch that. Player 1 can go first');
-		playGame();
 	}
+	choose();
 });
