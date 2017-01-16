e0ed723f489e9969913e478adb855e96baef55ed
rockinrobin714 <robindykema@gmail.com>
2017-01-15 20:21:33 -0600

    Check to make sure extra 0s aren't being added

--- calculator/js/javascript.js
+++ calculator/js/javascript.js
@@ -5,6 +5,7 @@ var operators = ['x','-','+','÷'];
 
 $('.num').click(function(){
 	currentString += $(this).html().replace(/^\s+|\s+$|\s+(?=\s)/g, "");
+	//check to make sure first digit isn't 0
 	while (currentString[0]==='0' && currentString.length>1){
 		currentString=currentString.substr(1);
 	}
@@ -13,12 +14,21 @@ $('.num').click(function(){
 
 $('#erase-one').click(function(){
 	//remove the last number from the string
-	currentString=currentString.substr(0, currentString.length-1);
-	//don't erase the last digit if it's 0
+	//check if the last button was an operator
+	if (operators.indexOf(currentString[currentString.length-1])>-1){
+
+	}else {
+		currentString = currentString.substr(0, currentString.length-1);
+	}
+	//if there is nothing left, set the string to 0
 	if (currentString===''){
 		currentString = '0';
 	}
 	$('#current-equation').html(currentString);
+})
+
+$('.operator').click(function(){
+	currentString += $(this).html().replace(/^\s+|\s+$|\s+(?=\s)/g, "");
 })
 
 //check phone to see how this works
