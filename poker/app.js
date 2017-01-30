4a543fc5a1c26e2f4f0aa78e8cf473c8f8f4686f
rockinrobin714 <robindykema@gmail.com>
2017-01-29 20:54:07 -0600

    Mapping table, adjusting style

--- poker/app.js
+++ poker/app.js
@@ -15,17 +15,7 @@ var Poker = function(){
     Q: 12,
     K: 13
     }
-    this.betsAndRankings = [
-    {royalFlush:{name: "Royal flush", 1:250, 2:500, 3:750, 4:1000, 5:4000}},
-    {straightFlugh: {name: "Straight flush", 1:50, 2:100, 3:150, 4: 200, 5:125}},
-    {fourOfAKind: {name: "Four of a kind", 1:25, 2:50, 3:75, 4:100, 5:125}},
-    {fullHouse:{name: "Full house", 1:9, 2:18, 3:27, 4:36, 5:45}},
-    {flush:{name: "Flush", 1:6, 2:12, 3:18, 4:24, 5: 30}},
-    {straight:{name: "Straight", 1:4, 2:8, 3:12, 4: 16, 5: 20}},
-    {threeOfAKind:{name: "Three of a kind",1:3,2:6,3:9,4:12,5:15}},
-    {twoPair:{name: "Two pair",1:2,2:4,3:6,4:8,5:10}},
-    {jacksOrBetter: {name:"Jacks or better", 1:1,2:2,3:3,4:4,5:5}}
-    ]
+    
   this.rankings = ["royalFlush","straightFlush"]
 
   this.deck = this.shuffleDeck(this.createDeck());
@@ -57,6 +47,25 @@ Poker.prototype.shuffleDeck = function(deck){
   }
  return deckCopy;
 }
-
-
+var betsAndRankings = [
+    {id: "royalFlush", name: "Royal flush", 1:250, 2:500, 3:750, 4:1000, 5:4000},
+    {id: "straightFlugh", name: "Straight flush", 1:50, 2:100, 3:150, 4: 200, 5:125},
+    {id: "fourOfAKind", name: "Four of a kind", 1:25, 2:50, 3:75, 4:100, 5:125},
+    {id: "fullHouse", name: "Full house", 1:9, 2:18, 3:27, 4:36, 5:45},
+    {id: "flush", name: "Flush", 1:6, 2:12, 3:18, 4:24, 5: 30},
+    {id: "straight", name: "Straight", 1:4, 2:8, 3:12, 4: 16, 5: 20},
+    {id: "threeOfAKind", name: "Three of a kind",1:3,2:6,3:9,4:12,5:15},
+    {id: "twoPair", name: "Two pair",1:2,2:4,3:6,4:8,5:10},
+    {id: "jacksOrBetter", name:"Jacks or better", 1:1,2:2,3:3,4:4,5:5}
+    ]
+for (var i=0;i<betsAndRankings.length;i++){
+  $('#table').append(`<tr>
+      <td class='title'>${betsAndRankings[i].name.toUpperCase()}</td>
+      <td>${betsAndRankings[i][1]}</td>
+      <td>${betsAndRankings[i][2]}</td>
+      <td>${betsAndRankings[i][3]}</td>
+      <td>${betsAndRankings[i][4]}</td>
+      <td>${betsAndRankings[i][5]}</td>
+    </tr>`)
+}
 var poker = new Poker();
